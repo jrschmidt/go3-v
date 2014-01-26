ef2b577dc77902202b38f1fa5c57aaa880c4434d
JR Schmidt <jrschmidt56@gmail.com>
2014-01-25 16:13:29 -0800

    add get_point and valid_point tests

--- unit_tests.rb
+++ unit_tests.rb
@@ -8,27 +8,25 @@ require 'test/unit'
 
 class Go3Test < Test::Unit::TestCase
 
-
   def app
     Sinatra::Application
   end
 
-  def test_equals
-    assert_equal(7,7)
-    assert_equal("ABCDE","ABCDE")
-    assert_equal("Hello John","Hello John")
+  def test_valid_point
+    game = Game.new
+    board = game.board
+    assert board.valid_point?([1,1])
   end
 
-  def test_scopes
+  def test_get_point
     game = Game.new
     points = game.board.points
-
-    assert_equal(points.get_point([1,1]),:empty)
-    assert_equal(points.get_point([6,1,]),:empty)
-    assert_equal(points.get_point([3,5]),:empty)
-    assert_equal(points.get_point([1,6]),:empty)
-    assert_equal(points.get_point([9,4]),:empty)
-    assert_equal(points.get_point([8,11]),:empty)
+    assert_equal points.get_point([1,1]), :empty
+    assert_equal points.get_point([6,1,]), :empty
+    assert_equal points.get_point([3,5]), :empty
+    assert_equal points.get_point([1,6]), :empty
+    assert_equal points.get_point([9,4]), :empty
+    assert_equal points.get_point([8,11]), :empty
   end
 
 end
