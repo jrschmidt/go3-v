3ac9c88a1bcac4c00646df81c0e1bbf5f34c4a12
JR Schmidt <jrschmidt56@gmail.com>
2014-02-08 09:40:22 -0800

    start branch to refactor tests to separate files

--- unit_tests.rb
+++ unit_tests.rb
@@ -2,8 +2,9 @@
 
 require '~/Desktop/go3/go3'
 
-
 require 'test/unit'
+
+require '~/Desktop/go3/legal_moves_test.rb'
 
 
 
@@ -516,91 +517,7 @@ class Go3Test < Test::Unit::TestCase
 
   # Tests Part Three - Legal Moves Methods
 
-  def test_find_legal_moves
-    game = Game.new
-    board = game.board
-    legal_moves = game.legal_moves
 
-    set_test_groups(board,3)
-
-    red_moves = legal_moves.find_legal_moves(:red)
-    white_moves = legal_moves.find_legal_moves(:white)
-    blue_moves = legal_moves.find_legal_moves(:blue)
-
-    assert_equal red_moves.size, 48
-    assert_equal white_moves.size, 48
-    assert_equal blue_moves.size, 46
-
-    # Most Important Test Points:
-    assert red_moves.include?([5,1])
-    assert white_moves.include?([5,1])
-    assert blue_moves.include?([5,1])
-
-    assert red_moves.include?([5,6])
-    assert white_moves.include?([5,6])
-    assert blue_moves.include?([5,6])
-
-    assert red_moves.include?([9,4])
-    assert white_moves.include?([9,4])
-    assert blue_moves.include?([9,4])
-
-    assert red_moves.include?([2,2])
-    assert white_moves.include?([2,2])
-    refute blue_moves.include?([2,2])
-
-    assert red_moves.include?([1,3])
-    assert white_moves.include?([1,3])
-    refute blue_moves.include?([1,3])
-
-    # Other Points:
-    assert red_moves.include?([2,7])
-    assert white_moves.include?([2,7])
-    assert blue_moves.include?([2,7])
-
-    assert red_moves.include?([7,6])
-    assert white_moves.include?([7,6])
-    assert blue_moves.include?([7,6])
-
-    assert red_moves.include?([9,7])
-    assert white_moves.include?([9,7])
-    assert blue_moves.include?([9,7])
-
-    assert red_moves.include?([11,6])
-    assert white_moves.include?([11,6])
-    assert blue_moves.include?([11,6])
-
-    assert red_moves.include?([6,11])
-    assert white_moves.include?([6,11])
-    assert blue_moves.include?([6,11])
-
-    assert red_moves.include?([10,11])
-    assert white_moves.include?([10,11])
-    assert blue_moves.include?([10,11])
-
-    assert red_moves.include?([7,9])
-    assert white_moves.include?([7,9])
-    assert blue_moves.include?([7,9])
-
-    refute red_moves.include?([3,2])
-    refute white_moves.include?([3,2])
-    refute blue_moves.include?([3,2])
-
-    refute red_moves.include?([1,4])
-    refute white_moves.include?([1,4])
-    refute blue_moves.include?([1,4])
-
-    refute red_moves.include?([6,4])
-    refute white_moves.include?([6,4])
-    refute blue_moves.include?([6,4])
-
-    refute red_moves.include?([5,5])
-    refute white_moves.include?([5,5])
-    refute blue_moves.include?([5,5])
-
-    refute red_moves.include?([2,6])
-    refute white_moves.include?([2,6])
-    refute blue_moves.include?([2,6])
-  end
 
 
   # Custom Assertion Methods
